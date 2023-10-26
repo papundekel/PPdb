252e235d2cf798b0b5c58fbf4e3c18323b639b5b
Peter Fačko <pfacko1@gmail.com>
2023-10-26 20:16:02 +0200

    bugfix: generate `openapi.json` in a temp dir

--- generate-frontend-api.sh
+++ generate-frontend-api.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 cd backend/ && \
-PYTHONPATH=backend/ python -m backend > openapi.json && \
+PYTHONPATH=backend/ python -m backend > ../tmp/openapi.json && \
 cd - && \
-openapi-generator generate --input-spec backend/openapi.json --generator-name typescript-fetch --output frontend/ppdb-frontend-api/src --additional-properties=importFileExtension=.js && \
+openapi-generator generate --input-spec tmp/openapi.json --generator-name typescript-fetch --output frontend/ppdb-frontend-api/src --additional-properties=importFileExtension=.js && \
 npm run --prefix frontend/ppdb-frontend-api/ build
